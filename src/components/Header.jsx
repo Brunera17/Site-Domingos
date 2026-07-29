00323196d2a3e71100b4dd7c3b9206fa6110ed4e
Bruno David Martins <dbrunobruno369@gmail.com>
2026-07-29 01:34:09 -0300

    atualização de links do menu e remoção de rota de trabalhe conosco

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ const navLinks = [
     { to: '/', label: 'Início' },
     { to: '/sobre', label: 'Sobre' },
     { to: '/servicos', label: 'Serviços' },
-    { to: '/blog', label: 'Blog' },
-    { to: '/trabalhe-conosco', label: 'Trabalhe Conosco' },
+//    { to: '/blog', label: 'Blog' },
+//    { to: '/trabalhe-conosco', label: 'Trabalhe Conosco' },
     { to: '/contato', label: 'Contato' },
 ]
 
