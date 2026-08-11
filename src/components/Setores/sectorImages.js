656bae4cd42375252b28673a070bb6ad8708d317
Bruno David Martins <dbrunobruno369@gmail.com>
2026-08-11 17:21:18 -0300

    Adição de arquivos de imagens dos setores e correção do nome da função exportada no arquivo SetoresSection.jsx.

---
+++ src/components/Setores/sectorImages.js
@@ -0,0 +1,19 @@
+import servicos from "../../assets/setor-servicos.jpg";
+import comercio from "../../assets/setor-comercio.jpg";
+import atacado from "../../assets/setor-atacado.jpg";
+import rural from "../../assets/setor-rural.jpg";
+import industria from "../../assets/setor-industria.jpg";
+import ecommerce from "../../assets/setor-ecommerce.jpg";
+import cooperativa from "../../assets/setor-cooperativa.jpg";
+import hospital from "../../assets/setor-hospital.jpg";
+
+export const sectorImages = {
+  'Prestadores de Serviços': servicos,
+  'Comércio': comercio,
+  'Atacados': atacado,
+  'Rural': rural,
+  'Indústrias': industria,
+  'E-commerce': ecommerce,
+  'Associações e Cooperativas': cooperativa,
+  'Hospitais': hospital,
+};
